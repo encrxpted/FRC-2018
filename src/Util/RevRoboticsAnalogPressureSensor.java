30ae375722278557865f59718e8e818e26c0512a
Harrison Hoytt <hhhoytt@gmail.com>
2018-01-23 16:31:36 -0500

    Added script for the Rev Robotics analog sensor

---
+++ src/Util/RevRoboticsAnalogPressureSensor.java
@@ -0,0 +1,21 @@
+package Util;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+
+/**
+ * Wraps an analog input for a Rev Robotics Analog Pressure sensor.
+ *
+ * http://www.revrobotics.com/wp-content/uploads/2015/11/REV-11-1107-DS-00.pdf
+ */
+public class RevRoboticsAnalogPressureSensor {
+    private final AnalogInput mAnalogInput;
+
+    public RevRoboticsAnalogPressureSensor(int analogInputNumber) {
+        mAnalogInput = new AnalogInput(analogInputNumber);
+    }
+
+    public double getAirPressurePsi() {
+        // taken from the datasheet
+        return 250.0 * mAnalogInput.getVoltage() / 5.0 - 25.0;
+    }
+}
