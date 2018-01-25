e4308328754e4615622321fad5f805413a73f10b
encrxpted <rulangu@gmail.com>
2018-01-25 16:19:41 -0500

    Added last year's methods

--- src/main/subsystems/Drivetrain.java
+++ src/main/subsystems/Drivetrain.java
@@ -1,7 +1,7 @@
 package main.subsystems;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-
 import Util.DriveHelper;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -13,63 +13,137 @@ import main.commands.drivetrain.Drive;
 
 public class Drivetrain extends Subsystem implements Constants, HardwareAdapter {
 	private static DifferentialDrive driveTrain = new DifferentialDrive(leftDriveMaster, rightDriveMaster);
-	
+	//TELEOP DRIVING
 	private DriveHelper helper = new DriveHelper(7.5);
-	private double lastTime = 0.0; // the last time of the call of driveVelocity()
+	private double lastTime = 0.0;
+	
+	//SHIFTING
+	private static boolean highGearState = false;
+	
+	//THESE GO INTO CONSTANTS DEPENDING ON IF THIS CODE IS PUSHED
+	public final boolean ENCODERS_INVERTED = true;
+	public final int ENCODER_RESET_TIMEOUT = 10;
+	public final int ENCODER_DEFAULTS_TIMEOUT = 0;
+	public final int timeoutMs = 10; //timeout for sensor methods. 10ms will allow checking to be performed.
 
 	public Drivetrain() {
 		setTalonDefaults();
 	}
 	
-	/**
-	 * TODO: what does this method do?
-	 * 
-	 * @param throttle throttle
-	 * @param heading heading
-	 */
+	//DRIVE FOR TELEOP
 	public void driveVelocity(double throttle, double heading) {
 		double currentTime;
-		
+
 		setBrakeMode(BRAKE_MODE);
 		System.out.println("Throttle: " + throttle + " | Heading: " + heading);
 		driveTrain.arcadeDrive(helper.driveSmooth(throttle), helper.handleOverPower(helper.handleDeadband(heading, headingDeadband)));
-		// TODO: get ride of the comment
-		//driveTrain.tankDrive
 		currentTime = Timer.getFPGATimestamp();
 		SmartDashboard.putNumber("Milliseconds between each call", currentTime-lastTime);
 		lastTime = currentTime;
 	}
 	
-	/**
-	 * Inverts the hbridge output of left & right dive master & slave 1.
-	 * 
-	 * @param isInverted
-	 */
+	/*************************
+	 * SENSOR OUTPUT METHODS *
+	 *************************/
+	//SET CONVERSION FACTOR AND WHEEL SIZE BEFORE USING THESE METHODS
+//	public int convertToEncoderTicks(double displacement) {//ft
+//		return (int) (((displacement / (wheelSize*Math.PI)) * conversionFactor));
+//	}
+//	public double getDistanceTraveledLeft() {//Feet
+//		return wheelSize*Math.PI*(getLeftEncoderPosition()/conversionFactor);
+//	}
+//	
+//	public double getDistanceTraveledRight() {//Feet
+//		//Removed - value and changed with reverseSensor() so that pid has correct feedback
+//		//System.out.println("r" +wheelSize*Math.PI*(getRightEncoderPosition()/conversionFactor));
+//		return wheelSize*Math.PI*(getRightEncoderPosition()/conversionFactor);
+//	}
+//	
+//	public double getDistanceAvg() {
+//		return (-getDistanceTraveledLeft() + getDistanceTraveledRight())/2; 
+//	}
+	
+	public double getLeftVelocity() {
+		return leftDriveMaster.getSensorCollection().getQuadratureVelocity();// / wheelEncoderMult;
+	}
+	
+	public double getRightVelocity() {
+		return rightDriveMaster.getSensorCollection().getQuadratureVelocity();// / wheelEncoderMult;
+	}
+	
+	@SuppressWarnings("unused")
+	private double getLeftEncoderPosition() {
+		return leftDriveMaster.getSensorCollection().getQuadraturePosition();
+	}
+	
+	@SuppressWarnings("unused")
+	private double getRightEncoderPosition() {
+		return rightDriveMaster.getSensorCollection().getQuadraturePosition();
+	}
+	
+	/**************************
+	 * SENSOR SUPPORT METHODS *
+	 **************************/
+	public void resetEncoders(int timeoutMs) {
+		leftDriveMaster.getSensorCollection().setQuadraturePosition(0, timeoutMs); //Alex is gay
+		rightDriveMaster.getSensorCollection().setQuadraturePosition(0, timeoutMs); //Alex is gay
+		//he said it, not me 
+		}
+	
+	public void resetSensors() {
+//		resetGyro(); 
+		resetEncoders(ENCODER_RESET_TIMEOUT);
+	}
+	
+	@SuppressWarnings("unused")
+	private void setFeedBackDefaults(int timeoutMs) {
+		leftDriveMaster.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, timeoutMs);
+		rightDriveMaster.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, timeoutMs);
+		//I don't think the coder per rev thing is needed anymore...
+		// "Units are 4X measurements where X units = X quadrature edges. Measured in change per 100ms."
+//		leftDriveMaster.configEncoderCodesPerRev(codesPerRev);
+//		rightDriveMaster.configEncoderCodesPerRev(codesPerRev);
+		leftDriveMaster.setSensorPhase(ENCODERS_INVERTED);
+		rightDriveMaster.setSensorPhase(ENCODERS_INVERTED);
+	}
+	
+	//TODO: CONVERT METHODS INTO NEW ONES
+//	private void setVoltageDefaults() {
+		// NO LONGER MEASURED IN VOLTS. MEASURES THROTTLE IN [-1, 1]
+//		leftDriveMaster.configNominalOutputVoltage(+0f, -0f);
+//		rightDriveMaster.configNominalOutputVoltage(+0f, -0f);
+//		leftDriveMaster.configPeakOutputVoltage(+12f, -12f);
+//		rightDriveMaster.configPeakOutputVoltage(+12f, -12f);
+//	}
+
+//	private void setRampRate(double ramp) {
+//		leftDriveMaster.setVoltageCompensationRampRate(ramp);
+//		rightDriveMaster.setVoltageCompensationRampRate(ramp);
+//	}
+
+	/*************************
+	 * DRIVE SUPPORT METHODS *
+	 *************************/
+	
+	public void changeGearing(){
+		highGearState = !highGearState;
+	}	
+	
 	private void reverseTalons(boolean isInverted) {
 		leftDriveMaster.setInverted(isInverted);
 		rightDriveMaster.setInverted(isInverted);
 		leftDriveSlave1.setInverted(isInverted);
 		rightDriveSlave1.setInverted(isInverted);
 	}
-	
-	/**
-	 * Sets brake mode for left & right dive master & slave 1.
-	 * 
-	 * @param mode mode
-	 */
+
 	private void setBrakeMode(NeutralMode mode) {
 		leftDriveMaster.setNeutralMode(mode);
 		leftDriveSlave1.setNeutralMode(mode);
 		rightDriveMaster.setNeutralMode(mode);
 		rightDriveSlave1.setNeutralMode(mode);
 	}
-	
-	/**
-	 * Sets control mode.
-	 */
+
 	private void setCtrlMode() {
-		// TODO: fix this
-		// Problem Here BOI'S
 		/*
 		leftDriveMaster.set(PERCENT_VBUS_MODE, 0.5);     
 		rightDriveMaster.set(PERCENT_VBUS_MODE, 0.5);
@@ -80,21 +154,17 @@ public class Drivetrain extends Subsystem implements Constants, HardwareAdapter 
 		rightDriveSlave1.follow(rightDriveMaster);
 	}
 	
-	/**
-	 * Sets Talon defaults.
-	 */
+
 	public void setTalonDefaults() {
 		reverseTalons(false);
 		setBrakeMode(BRAKE_MODE);
 		setCtrlMode();
 	}
 
-	/**
-	 * Inits default command.
-	 */
 	@Override
 	protected void initDefaultCommand() {
 		setDefaultCommand(new Drive());
 	}
 	
 }
+
