1918b28ec4afccdbc391e32c8e0653d8bc56bc2e
5ARAH101 <32044328+5ARAH101@users.noreply.github.com>
2018-01-25 20:48:03 -0500

    Added stuff to intake

--- src/main/subsystems/Intake.java
+++ src/main/subsystems/Intake.java
@@ -1,5 +1,8 @@
 package main.subsystems;
 
-public class Intake {
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+public class Intake extends Climber {
+	public static WPI_TalonSRX DriveMaster = new WPI_TalonSRX(intakeMaster);
+	
 }
