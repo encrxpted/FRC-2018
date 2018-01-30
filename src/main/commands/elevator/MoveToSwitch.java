22b5e2463e4ef098176dac09b669ee65c7b9025f
Harrison Hoytt <hhhoytt@gmail.com>
2018-01-29 20:25:28 -0500

    Fixed errors

--- src/main/commands/elevator/MoveToSwitch.java
+++ src/main/commands/elevator/MoveToSwitch.java
@@ -4,60 +4,36 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 import main.Constants;
 import main.HardwareAdapter;
 
-/**
- *
- */
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import main.Constants;
+import main.HardwareAdapter;
+import main.Robot;
+
 public class MoveToSwitch extends CommandGroup implements Constants, HardwareAdapter {
-
-    public MoveToSwitch() {
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
-
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
-
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
+	public void MoveToSwich() {
+		requires(Robot.it);
+	}
+	
+    // Called just before this Command runs the first time
+    protected void initialize() {
     }
-    import edu.wpi.first.wpilibj.command.CommandGroup;
-    import main.Constants;
-    import main.HardwareAdapter;
-    import main.Robot;
 
-    public class MoveToSwitch extends CommandGroup implements Constants, HardwareAdapter {
-    	public MoveToSwich() {
-    		requires(Robot.it);
-    	}
-    	
-        // Called just before this Command runs the first time
-        protected void initialize() {
-        }
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    
+    }
 
-        // Called repeatedly when this Command is scheduled to run
-        protected void execute() {
-        
-        }
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return true;
+    }
 
-        // Make this return true when this Command no longer needs to run execute()
-        protected boolean isFinished() {
-            return true;
-        }
+    // Called once after isFinished returns true
+    protected void end() {
+    }
 
-        // Called once after isFinished returns true
-        protected void end() {
-        }
-
-        // Called when another command which requires one or more of the same
-        // subsystems is scheduled to run
-        protected void interrupted() {
-        }
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
     }
 }
