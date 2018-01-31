5ce14a59d0ae599814d0055f9f3dd01959dcee30
encrxpted <rulangu@gmail.com>
2018-01-30 19:15:11 -0500

    Worked on Intake switches and setting them

--- src/main/subsystems/Intake.java
+++ src/main/subsystems/Intake.java
@@ -12,27 +12,34 @@ public class Intake extends Subsystem implements Constants, HardwareAdapter {
 		In, Out, Off
 	}
 	
+	// NOT SURE IF WE SHOULD USE AN ENUM- TESTING SWITCH METHODS FIRST
+//	public static enum CubeInIntake {
+//		HasCube, NoCube
+//	}
+	
 	public static WheelStates wheelStates = WheelStates.Off;
+	//public static CubeInIntake cubeInIntake = CubeInIntake.NoCube;
 	
 	public void spinIn() {
 		leftIntakeMotor.set(1.0);
     	rightIntakeMotor.set(-1.0);
     	wheelStates = WheelStates.In;
-
 	}
 	
 	public void spinOut() {
 		leftIntakeMotor.set(-1.0);
     	rightIntakeMotor.set(1.0);
     	wheelStates = WheelStates.Out;
-
 	}
 	
 	public void spinOff() {
     	leftIntakeMotor.set(0.0);
     	rightIntakeMotor.set(0.0);
     	wheelStates = WheelStates.Off;
-
+	}
+	
+	public boolean isCubeInIntake() {
+		return intakeSwitch.get();
 	}
 	
 	@Override
