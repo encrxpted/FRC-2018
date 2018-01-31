177f9eb9566882f277d3ce76fef24ccb1f6d29a9
Programming Team <Programming Team@DESKTOP-SSKQP4N.knoxschools.ad>
2018-01-30 19:34:56 -0500

    wa

--- src/main/commands/driveAlerts/AlertDriver.java
+++ src/main/commands/driveAlerts/AlertDriver.java
@@ -1,5 +1,20 @@
 package main.commands.driveAlerts;
 
-public class AlertDriver {
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
+import main.Constants;
 
+public class AlertDriver extends CommandGroup implements Constants {
+	public AlertDriver() {// Utilize pulseAlertLight method to simplify this.
+		for (int i = 0; i < 4; i++) {
+			addSequential(new AlertLightOnForTime(alertOnTime));
+			addSequential(new WaitCommand(alertOffTime));
+	    }
+	}
+
+	private void addSequential(AlertLightOnForTime alertLightOnForTime) {
+		// TODO Auto-generated method stub
+		
+	}
 }
+
