96dd0024c73ec15e064a66818f18ae95e945ce65
matthewbillings <32077912+matthewbillings@users.noreply.github.com>
2018-02-09 20:07:45 -0500

    add more stuff to drop

--- src/main/commands/commandgroups/Dropwhenpressed.java
+++ src/main/commands/commandgroups/Dropwhenpressed.java
@@ -1,5 +1,20 @@
 package main.commands.commandgroups;
 
-public class Dropwhenpressed {
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
+import main.Constants;
+import main.HardwareAdapter;
+import main.commands.pnuematics.Arm;
+import main.commands.pnuematics.Tilt;
 
+public class  Dropwhenpressed extends CommandGroup implements Constants, HardwareAdapter{
+	    // Called just before this Command runs the first time
+	    public Dropwhenpressed() {
+	    	addSequential(new Tilt(EXT));
+	    	addSequential(new WaitCommand(0.1));
+	    	addSequential(new Tilt(OFF));
+	    	addSequential(new Arm(RET));
+	    	addSequential(new WaitCommand(0.1));
+	    	addSequential(new Arm(OFF)); 
+	    }
 }
