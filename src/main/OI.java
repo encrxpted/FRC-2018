80beac9a3cb4def85dd8d5249d574b5cb6a9b434
RoshanVarma <roshanvarma312@gmail.com>
2018-02-11 17:03:11 -0500

    robot now drives... always check to make sure rio is wired correctly

--- src/main/OI.java
+++ src/main/OI.java
@@ -70,10 +70,10 @@ public class OI extends CommandGroup implements Constants, HardwareAdapter {
 			//xbox.leftTrigger.whenPressed(new TiltUp());
 			//xbox.rightTrigger.whenPressed(new TiltDown());
 			// intake
-			/*xbox.a.whenPressed(new SpinIn());
-			xbox.a.whenReleased(new SpinOff());*/
-			xbox.a.whenReleased(new IntakeCubeOff());
-			xbox.a.whenPressed(new IntakeCube());
+			xbox.a.whenPressed(new SpinIn());
+			xbox.a.whenReleased(new SpinOff());
+			/*xbox.a.whenReleased(new IntakeCubeOff());
+			xbox.a.whenPressed(new IntakeCube());*/
 			
 			xbox.y.whenPressed(new PushOutCube());
 			xbox.y.whenReleased(new PushOutCubeOff());
