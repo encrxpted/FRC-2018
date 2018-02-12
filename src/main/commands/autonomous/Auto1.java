a8a2f745633bcee8cd9281100e6cc3f25f532b8b
TheSuperDuck <TheSuperDuck@users.noreply.github.com>
2018-02-12 17:37:32 -0500

    Removed void from constructor

--- src/main/commands/autonomous/Auto1.java
+++ src/main/commands/autonomous/Auto1.java
@@ -2,12 +2,10 @@ package main.commands.autonomous;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import main.commands.drivetrain.DriveForward;
-import main.commands.drivetrain.Drive;
-
 
 public class Auto1 extends CommandGroup{
 
-	public void Auto1() {
+	public Auto1() {
 		addSequential(new DriveForward(1.5));
 	}
 }
