60982f252ef66d66b35d3f82d379166651338859
CryptoMiner <CryptoMiner@192.168.2.2>
2018-02-17 20:18:17 -0800

    Fixed code tabing and spacing

--- src/interfacesAndAbstracts/ImprovedSendableChooser.java
+++ src/interfacesAndAbstracts/ImprovedSendableChooser.java
@@ -8,53 +8,52 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
 import static java.util.Objects.requireNonNull;
 
 public class ImprovedSendableChooser<V> extends SendableBase implements Sendable {
-  private static final String DEFAULT = "default";
-  private static final String SELECTED = "selected";
-  private static final String OPTIONS = "options";
-  private final LinkedHashMap<String, V> m_map = new LinkedHashMap<>();
-  private String m_defaultChoice = "";
-  
-  public ImprovedSendableChooser() {
-  }
-  
-  public void addObject(String name, V object) {
-    m_map.put(name, object);
-  }
-  
-  public void removeObject(String name, V object) {
-	  m_map.remove(name, object);
-  }
+	private static final String DEFAULT = "default";
+	private static final String SELECTED = "selected";
+	private static final String OPTIONS = "options";
+	private final LinkedHashMap<String, V> m_map = new LinkedHashMap<>();
+	private String m_defaultChoice = "";
 
-  public void addDefault(String name, V object) {
-    requireNonNull(name, "Provided name was null");
-    m_defaultChoice = name;
-    addObject(name, object);
-  }
+	public ImprovedSendableChooser() {
+	}
 
-  public V getSelected() {
-	String selected = m_defaultChoice;
+	public void addObject(String name, V object) {
+		m_map.put(name, object);
+	}
 
-    if (m_tableSelected != null) {
-      selected = m_tableSelected.getString(m_defaultChoice);
-    }
-    
-    return m_map.get(selected);
-  }
-  
-  private NetworkTableEntry m_tableSelected;
-  
-  @Override
-  public void initSendable(SendableBuilder builder) {
-    builder.setSmartDashboardType("String Chooser");
-    builder.addStringProperty(DEFAULT, () -> {
-      return m_defaultChoice;
-    }, null);
+	public void removeObject(String name, V object) {
+		m_map.remove(name, object);
+	}
 
-    builder.addStringArrayProperty(OPTIONS, () -> {
-      return m_map.keySet().toArray(new String[0]);
-    }, null);
+	public void addDefault(String name, V object) {
+		requireNonNull(name, "Provided name was null");
+		m_defaultChoice = name;
+		addObject(name, object);
+	}
 
-    m_tableSelected = builder.getEntry(SELECTED);
-  }
+	public V getSelected() {
+		String selected = m_defaultChoice;
+
+		if (m_tableSelected != null) {
+			selected = m_tableSelected.getString(m_defaultChoice);
+		}
+
+		return m_map.get(selected);
+	}
+
+	private NetworkTableEntry m_tableSelected;
+
+	@Override
+	public void initSendable(SendableBuilder builder) {
+		builder.setSmartDashboardType("String Chooser");
+		builder.addStringProperty(DEFAULT, () -> {
+			return m_defaultChoice;
+		}, null);
+
+		builder.addStringArrayProperty(OPTIONS, () -> {
+			return m_map.keySet().toArray(new String[0]);
+		}, null);
+
+		m_tableSelected = builder.getEntry(SELECTED);
+	}
 }
-
