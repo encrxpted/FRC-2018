3e61017d54d91f7019848acaad17cd66ae97b4e7
Programming Team <Programming Team@DESKTOP-SSKQP4N.knoxschools.ad>
2018-02-20 16:21:37 -0500

    add dance to the code

--- src/main/OI.java
+++ src/main/OI.java
@@ -3,6 +3,8 @@ package main;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 import lib.joystick.XboxController;
+import main.commands.commandgroups.Dance;
+import main.commands.commandgroups.DanceOff;
 import main.commands.commandgroups.DropCube;
 import main.commands.commandgroups.DropCubeOff;
 import main.commands.commandgroups.IntakeCube;
@@ -18,6 +20,8 @@ public class OI extends CommandGroup implements Constants, HardwareAdapter {
 	public OI() {
 		xbox.setInternalControl(false);
 		xbox2.setInternalControl(false);
+		xbox.start.whileHeld(new Dance());
+	    xbox.start.whileHeld(new DanceOff());
 		
 		xbox.leftBumper.whenPressed(new ShiftUp());
 		xbox.leftBumper.whenReleased(new ShiftDown());
