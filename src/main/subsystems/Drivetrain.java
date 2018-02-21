b65df77855fd05895b49597eac26933e3d02e66d
Programming Team <Programming Team@DESKTOP-V2RU8NF.knoxschools.ad>
2018-02-20 21:37:26 -0500

    commented out 2nd drivetrain slaves and made getVoltage() an average

--- src/main/subsystems/Drivetrain.java
+++ src/main/subsystems/Drivetrain.java
@@ -54,11 +54,11 @@ public class Drivetrain extends RobotSubsystem  {
 	 * PLAY/RECORD METHODS *
 	 ***********************/
 	public double getLeftVoltage() {
-		return leftDriveMaster.getMotorOutputVoltage();
+		return (leftDriveMaster.getMotorOutputVoltage() + leftDriveSlave1.getMotorOutputVoltage())/2;
 	}
 	
 	public double getRightVoltage() {
-		return rightDriveMaster.getMotorOutputVoltage();
+		return (rightDriveMaster.getMotorOutputVoltage() + rightDriveSlave1.getMotorOutputVoltage())/2;
 	}
 	
 	/*************************
@@ -68,66 +68,66 @@ public class Drivetrain extends RobotSubsystem  {
 		leftDriveMaster.setInverted(isInverted);
 		rightDriveMaster.setInverted(isInverted);
 		leftDriveSlave1.setInverted(isInverted);
-		leftDriveSlave2.setInverted(!isInverted);
+		//leftDriveSlave2.setInverted(!isInverted);
 		rightDriveSlave1.setInverted(isInverted);
-		rightDriveSlave2.setInverted(!isInverted);
+		//rightDriveSlave2.setInverted(!isInverted);
 	}
 
 	private void setBrakeMode(NeutralMode mode) {
 		leftDriveMaster.setNeutralMode(mode);
 		leftDriveSlave1.setNeutralMode(mode);
-		leftDriveSlave2.setNeutralMode(mode);
+		//leftDriveSlave2.setNeutralMode(mode);
 		rightDriveMaster.setNeutralMode(mode);
 		rightDriveSlave1.setNeutralMode(mode);
-		rightDriveSlave2.setNeutralMode(mode);
+		//rightDriveSlave2.setNeutralMode(mode);
 	}
 
 	private void setCtrlMode() {
 		leftDriveSlave1.follow(leftDriveMaster);
 		rightDriveSlave1.follow(rightDriveMaster);
-		leftDriveSlave2.follow(leftDriveMaster);
-		rightDriveSlave2.follow(rightDriveMaster);
+		//leftDriveSlave2.follow(leftDriveMaster);
+		//rightDriveSlave2.follow(rightDriveMaster);
 	}
 	
 	private void setVoltageComp(boolean set, double voltage, int timeout) {
 		//Voltage Compensation
 		leftDriveMaster.enableVoltageCompensation(set);
 		leftDriveSlave1.enableVoltageCompensation(set);
-		leftDriveSlave2.enableVoltageCompensation(set);
+//		leftDriveSlave2.enableVoltageCompensation(set);
 		rightDriveMaster.enableVoltageCompensation(set);
 		rightDriveSlave1.enableVoltageCompensation(set);
-		rightDriveSlave2.enableVoltageCompensation(set);
+//		rightDriveSlave2.enableVoltageCompensation(set);
 		leftDriveMaster.configVoltageCompSaturation(voltage, timeout);
 		leftDriveSlave1.configVoltageCompSaturation(voltage, timeout);
-		leftDriveSlave2.configVoltageCompSaturation(voltage, timeout);
+//		leftDriveSlave2.configVoltageCompSaturation(voltage, timeout);
 		rightDriveMaster.configVoltageCompSaturation(voltage, timeout);
 		rightDriveSlave1.configVoltageCompSaturation(voltage, timeout);
-		rightDriveSlave2.configVoltageCompSaturation(voltage, timeout);
+//		rightDriveSlave2.configVoltageCompSaturation(voltage, timeout);
 		//Nominal and peak outputs
 		leftDriveMaster.configPeakOutputForward(1.0, timeout);
 		leftDriveSlave1.configPeakOutputForward(1.0, timeout);
-		leftDriveSlave2.configPeakOutputForward(1.0, timeout);
+//		leftDriveSlave2.configPeakOutputForward(1.0, timeout);
 		rightDriveMaster.configPeakOutputForward(1.0, timeout);
 		rightDriveSlave1.configPeakOutputForward(1.0, timeout);
-		rightDriveSlave2.configPeakOutputForward(1.0, timeout);
+//		rightDriveSlave2.configPeakOutputForward(1.0, timeout);
 		leftDriveMaster.configPeakOutputReverse(-1.0, timeout);
 		leftDriveSlave1.configPeakOutputReverse(-1.0, timeout);
-		leftDriveSlave2.configPeakOutputReverse(-1.0, timeout);
+//		leftDriveSlave2.configPeakOutputReverse(-1.0, timeout);
 		rightDriveMaster.configPeakOutputReverse(-1.0, timeout);
 		rightDriveSlave1.configPeakOutputReverse(-1.0, timeout);
-		rightDriveSlave2.configPeakOutputReverse(-1.0, timeout);
+//		rightDriveSlave2.configPeakOutputReverse(-1.0, timeout);
 		leftDriveMaster.configNominalOutputForward(0.0, timeout);
 		leftDriveSlave1.configNominalOutputForward(0.0, timeout);
-		leftDriveSlave2.configNominalOutputForward(0.0, timeout);
+//		leftDriveSlave2.configNominalOutputForward(0.0, timeout);
 		rightDriveMaster.configNominalOutputForward(0.0, timeout);
 		rightDriveSlave1.configNominalOutputForward(0.0, timeout);
-		rightDriveSlave2.configNominalOutputForward(0.0, timeout);
+//		rightDriveSlave2.configNominalOutputForward(0.0, timeout);
 		leftDriveMaster.configNominalOutputReverse(0.0, timeout);
 		leftDriveSlave1.configNominalOutputReverse(0.0, timeout);
-		leftDriveSlave2.configNominalOutputReverse(0.0, timeout);
+//		leftDriveSlave2.configNominalOutputReverse(0.0, timeout);
 		rightDriveMaster.configNominalOutputReverse(0.0, timeout);
 		rightDriveSlave1.configNominalOutputReverse(0.0, timeout);
-		rightDriveSlave2.configNominalOutputReverse(0.0, timeout);
+//		rightDriveSlave2.configNominalOutputReverse(0.0, timeout);
 	}
 	
 	public void setTalonDefaults() {
