4cbceb8df0088a1c88a89039b3b487b7770c520c
encrxpted <rulangu@gmail.com>
2018-03-07 00:07:05 -0500

    driving to 5 rotations implementing...

--- src/main/commands/drivetrain/DriveToDistance.java
+++ src/main/commands/drivetrain/DriveToDistance.java
@@ -1,14 +1,29 @@
 package main.commands.drivetrain;
 
+import Util.GeneratedMotionProfile;
 import interfacesAndAbstracts.ImprovedCommand;
+import main.Robot;
 
 public class DriveToDistance extends ImprovedCommand {
 	
-	//TODO THIS
+	protected void initialize() {
+		Robot.dt.resetMP();
+		Robot.dt.setMPMode(MPDisable);
+    	Robot.oi.setInternalControl(true);
+    	Robot.dt.fillMPE(GeneratedMotionProfile.Points, GeneratedMotionProfile.Points);
+	}
+	
+	protected void execute() {
+		Robot.dt.setMPMode(MPEnable);
+	}
+	
+	protected void end() {
+		Robot.dt.setMPMode(MPDisable);
+	}
 
 	@Override
 	protected boolean isFinished() {
-		return false;
+		return false; //TODO THIS
 	}
 
 }
